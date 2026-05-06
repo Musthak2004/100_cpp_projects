1a50371bdf3a1320225731e70ad3aef32adac8a0
Musthak <musthakcool55@gmail.com>
2026-05-06 17:20:47 +0530

    initial commit

--- Beginner level/simpleInterestCalculator.cpp
+++ Beginner level/simpleInterestCalculator.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+    double simpleInterestForYear;
+    double simpleInterestForMonth;
+    double principalAmount;
+    double interestRatePerYear;
+    double timeInYears;
+
+    cout << "***** Simple Interest Calculator *****\n";
+
+    cout << "\n";
+
+    cout << "Enter prinicipal amount: Rs.";
+    cin >> principalAmount;
+
+    cout << "Enter interest rate per year: ";
+    cin >> interestRatePerYear;
+
+    cout << "Enter time in years: ";
+    cin >> timeInYears;
+
+    cout << "\n";
+
+    simpleInterestForYear = (principalAmount * interestRatePerYear * timeInYears) / (100);
+    simpleInterestForMonth = simpleInterestForYear / 12;
+
+    cout << "Your Interest for month: Rs." << setprecision(2) << fixed << simpleInterestForMonth << "\n";
+    cout << "Your Interest for year: Rs." << setprecision(2) << fixed << simpleInterestForYear << "\n";
+
+    cout << "\n";
+
+    cout << "**************************************\n";
+
     return 0;
 }
