23c6d0568f04d3eba2eb402eaf176d27bd6dec0d
Musthak <musthakcool55@gmail.com>
2026-05-20 14:44:40 +0530

    initial commit

--- Beginner level/quizGame.cpp
+++ Beginner level/quizGame.cpp
@@ -10,9 +10,63 @@ int main()
         "4. Which planet is known as the Red Planet?",
         "5. What is 5 + 5?"};
 
-    string options[][4] = {
-        {}};
-    char answers[] = {};
+    string options[][4] =
+        {
+            {"A. Seoul", "B. Tokyo", "C. Beijing", "D. Bangkok"},
+
+            {"A. HTML", "B. C++", "C. Python", "D. Java"},
+
+            {"A. 5", "B. 6", "C. 7", "D. 8"},
+
+            {"A. Earth", "B. Jupiter", "C. Mars", "D. Venus"},
+
+            {"A. 8", "B. 9", "C. 10", "D. 11"}};
+
+    char answers[] = {'B', 'A', 'C', 'C', 'C'};
+
+    int size = sizeof(questions) / sizeof(questions[0]);
+
+    char guess;
+    int score = 0;
+
+    cout << "===== QUIZ GAME =====\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "\n";
+        cout << questions[i] << "\n\n";
+
+        for (int j = 0; j < 4; j++)
+        {
+            cout << options[i][j] << "\n";
+        }
+
+        cout << "\nEnter your answer (A/B/C/D): ";
+        cin >> guess;
+
+        guess = toupper(guess);
+
+        if (guess == answers[i])
+        {
+            cout << "Correct!\n";
+            score++;
+        }
+        else
+        {
+            cout << "Wrong!\n";
+            cout << "Correct answer was: " << answers[i] << "\n";
+        }
+    }
+
+    cout << "\n===== RESULT =====\n";
+
+    cout << "Your score: " << score << "/" << size << "\n";
+
+    double percentage = ((double)score / size) * 100;
+
+    cout << "Percentage: " << percentage << "%\n";
+
+    cout << "\n===================\n";
 
     return 0;
 }
