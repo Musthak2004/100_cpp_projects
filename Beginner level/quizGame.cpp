c9546224388349e1629a407f161bcee4037ab9d2
Musthak <musthakcool55@gmail.com>
2026-05-20 11:04:45 +0530

    initial commit

---
+++ Beginner level/quizGame.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string questions[5] = {
+        "1. What is the capital of Japan?",
+        "2. Which language is used for in web pages?",
+        "3. How many days are there in a week?",
+        "4. Which planet is known as the Red Planet?",
+        "5. What is 5 + 5?"};
+
+    string options[][4] = {
+        {}};
+    char answers[] = {};
+
+    return 0;
+}
